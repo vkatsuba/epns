12eecd4baa528d4c68bd20a5c7a05f90306400c5
vkatsuba <v.katsuba.dev@gmail.com>
2021-01-31 16:53:02 +0200

    Refactoring: remove non use function recv/2 for debug response from APNS

--- src/epns.erl
+++ src/epns.erl
@@ -39,7 +39,6 @@
 %%% ==================================================================
 
 -define(COMMAND_REQ, 1).
--define(COMMAND_RESP, 8).
 -define(EXPIRY, (os:system_time(second) + 604800)).
 -define(TOKEN_LENGTH, 32).
 -define(TIMEOUT, 500).
@@ -84,54 +83,3 @@ apns(#{playload := P, key := K, cert := C, url := U, token := T}) ->
         {error, Reason} ->
             {error, Reason}
     end.
-
-%% -------------------------------------------------------------------
-%% @doc
-%% Receive errors of APNS
-%% Use inside of push/1: ssl:controlling_process(Socket, spawn(fun() -> recv(self(), 100) end))
-%% @end
-%% -------------------------------------------------------------------
--spec recv(Pid :: pid(), Timeout :: integer()) -> Result :: term().
-
-recv(Pid, Timeout) ->
-    receive
-        {ssl, Sock, <<?COMMAND_RESP:8, Status, UserID:32/big>>} ->
-            io:format("APNS Error: [~p:~p/1]: Reason: ~p, message ID: ~p~n", [?MODULE, push, convert_status(Status), UserID]),
-            ssl:close(Sock),
-            Pid ! {error, UserID};
-        {ssl_closed, _} -> ok
-            after Timeout -> exit(Pid, normal)
-    end.
-
-%% -------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handler for status code of response from APNS
-%% @end
-%% -------------------------------------------------------------------
--spec convert_status(StatusCode :: integer()) -> Result :: binary().
-
-convert_status(0) ->
-    <<"no errors">>;
-convert_status(1) ->
-    <<"processing error">>;
-convert_status(2) ->
-    <<"missing device token">>;
-convert_status(3) ->
-    <<"missing topic">>;
-convert_status(4) ->
-    <<"missing payload">>;
-convert_status(5) ->
-    <<"invalid token size">>;
-convert_status(6) ->
-    <<"invalid topic size">>;
-convert_status(7) ->
-    <<"invalid payload size">>;
-convert_status(8) ->
-    <<"invalid token">>;
-convert_status(10) ->
-    <<"shutdown">>;
-convert_status(128) ->
-    <<"protocol error">>;
-convert_status(255) ->
-    <<"unknown">>.
